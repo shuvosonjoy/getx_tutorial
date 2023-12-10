2e68cc8c2fe0e9e45203f8cf4e7051f2c7f4eaf5
shuvosonjoy <shuvosdg123@gmail.com>
2023-12-10 15:27:16 +0600

    Revert "Dependency manager"
    This reverts commit 9be528b20cdd4801eb1261faf2d101db824a7d6b.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,63 @@
 import 'package:flutter/material.dart';
-import 'package:getx/counter_app.dart';
-import '';
+import 'package:get/get.dart';
+import 'coutner_state_controller.dart';
 
 void main() {
-  runApp(CounterApp());
+  runApp(myApp());
+}
+
+class GetxDependencyBinder extends Bindings{
+  @override
+  void dependencies() {
+  Get.put(CounterStateController());
+  }
+  
 }
 
 
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  GetMaterialApp(
+      home: HomeScreen(),
+      initialBinding: GetxDependencyBinder(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  CounterStateController counterStateController = Get.find<CounterStateController>();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GetBuilder<CounterStateController>(
+              builder: (controller) {
+                return Text(controller.count.toString());
+              }
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  counterStateController.updateValue(1);
+                },
+                child: Icon(Icons.add)),
+          ],
+        ),
+      ),
+    );
+  }
+}
